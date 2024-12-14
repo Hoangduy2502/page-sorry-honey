ca5e49e26bb4db9d946968e412e670ea26b14d21
Hoangduy2502 <85004219+Hoangduy2502@users.noreply.github.com>
2024-12-14 14:28:39 +0700

    test

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin lỗi người anh yêu!',
+    introDesc: `Người anh yêu là ĐINH THI MINH HẰNG`,
+    btnIntro: 'Ấn vào xem tí nhé con gà',
+    title: 'Có phải anh đã trêu con gà nhiều quá không 😙',
+    desc: 'Chắc không phải vì trêu tí mới vui mà con gà hay dỗi ghê ',
+    btnYes: 'Tha thứ lỗi lầm',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Không tha lỗi mà được với ta à sos',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Con gà con ',
+    btnAccept: 'Em là con gà',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
